b4446d9b62519e80363d00b36dee6c58d39b204c
Rafael Cunha <rafacunha.prof033@gmail.com>
2022-08-16 18:37:45 +0000

    Data types

--- Main.java
+++ Main.java
@@ -1,8 +1,20 @@
-
+// The '.java' file and the PUBLIC CLASS must have the same name
 
 
 public class Main {
+  
   public static void main(String[] args) {
-    System.out.println("Hello World!!");
+    // Primitive data types are byte, short, int, long, float, double, boolean and char
+    int myInt = 2;
+    float myFloat = 2.59f;
+    double float2Double = (double) myFloat;
+
+
+    System.out.println(
+      "Hello World!!" + 
+      "\n This is an int: " + myInt +
+      "\n Float: " + myFloat +
+      "\n Float to double: " + float2Double
+    );
   }
 }
