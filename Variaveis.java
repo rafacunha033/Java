3a9b7c649b84750fba66342f822781bb591aeb3d
Rafael Cunha <rafacunha.prof033@gmail.com>
2022-08-23 22:25:18 +0000

    Variaveis

---
+++ Variaveis.java
@@ -0,0 +1,27 @@
+/*
+Dados:
+Nome do aluno: Ian
+Primeira nota: 9,5
+Peso: 4
+Segunda nota: 9,0
+Peso: 6
+Faça um programa que exiba o nome do aluno e a sua média ponderada.
+*/
+
+
+class Variaveis {
+    public static void main(String[] args) {
+      String nomeAluno = "Ian";
+      
+      double nota1 = 9.5;
+      int peso1 = 4;
+      
+      double nota2 = 9.0;
+      int peso2 = 6;
+  
+      double media;
+  
+      media = ((nota1*peso1)+(nota2*peso2))/10;
+      System.out.println(media);
+    }
+}
