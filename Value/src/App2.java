b23fceeb5e4a880dc62af4313073bd41141bee22
Rafael Cunha <rafacunha.prof033@gmail.com>
2022-10-11 19:02:18 +0000

    App2 -> assignments by value and reference

--- Value/src/App2.java
+++ Value/src/App2.java
@@ -1,8 +1,31 @@
+import model.Product;
+
 public class App2 {
 
     public static void main(String[] args) 
     {
-        System.out.println("Teste");
-    
+        // primitive types -> assignment by value
+        // byte, short, int, long
+        // double, float
+        // boolean
+        // char
+
+        // The value of 'a' is passed to 'b', it means that a new memory address is created to 'b'
+        int a = 1;
+        int b = a;
+
+        a++;
+        b--;
+
+        System.out.println(a);
+        System.out.println(b);
+
+        // objects -> assignment by reference
+        // The value of 'p2' is a reference to 'p1', this means that p1 and p2 has the same memory address
+        Product p1 = new Product("Pencil", 5.99);
+        Product p2 = p1;
+
+        p2.name = "Pen";
+        System.out.println(p1.name);
     }
 }
